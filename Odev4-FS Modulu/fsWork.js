5839ed66e3daf65401c1c99f8065ee179cf78120
Doctor <75563269+seniordoctor@users.noreply.github.com>
2023-02-05 23:02:41 +0300

    rm

--- Odev4-FS Modulu/fsWork.js
+++ Odev4-FS Modulu/fsWork.js
@@ -3,7 +3,7 @@
 // 3- Bu veriyi güncelleyelim.
 // 4- Dosyayı silelim.
 
-// fs modulunu kullanmak icin;
+// // fs modulunu kullanmak icin;
 const fs = require('fs');
 
 // veri ekledik
@@ -14,21 +14,45 @@ fs.appendFile('employees.json',
 })
 
 // employees.json içindeki veriyi okuduk
-fs.readFile('employees.json', 'utf-8', (err, data) => {
-    if (err) console.log(err);
-    console.log(data)
-    console.log('json dosyası okundu');
-});
+    fs.readFile('employees.json', 'utf-8', (err, data) => {
+     if (err) console.log(err);
+     console.log(data)
+     console.log('json dosyası okundu');
+    });
 
 // employees.json'a yeni veri ekledik
-fs.appendFile('employees.json', 
-'\n {"name": "Employee 2 Name", "salary": 3333}', 'utf-8', (err) => {
-    if (err) console.log(err);
-    console.log('yeni json verisi olusturuldu')
-})
+        fs.appendFile('employees.json', 
+        '\n {"name": "Employee 2 Name", "salary": 3333}', 'utf-8', (err) => {
+            if (err) console.log(err);
+            console.log('yeni json verisi olusturuldu')
+        })
 
 // employees.json dosyasını sildik
-fs.unlink('employees.json', (err) => {
-    if (err) console.log(err);
-    console.log('json dosyası silindi')
-})
+            fs.unlink('employees.json', (err) => {
+                if (err) console.log(err);
+                console.log('json dosyası silindi')
+            })
+
+// Senkron Hali;
+
+// try {
+//     console.log("file creating...");
+//     appendFileSync("employees.json", '{"name": "RECEP", "salary": 1000}', "utf8")
+//     console.log("file created");
+    
+//     console.log("file reading...");
+//     let data = readFileSync("employees.json", "utf-8")
+//     console.log("data :", data);
+    
+//     console.log("file updating...");
+//     writeFileSync("employees.json", '{"name": "RECEP", "salary": 5000}', "utf8")
+//     data = readFileSync("employees.json", "utf-8")
+//     console.log("update success, data:", data);
+
+//     console.log("file deleting...");
+//     unlinkSync("employees.json")
+//     console.log("file deleted");
+
+// } catch (error) {
+//     console.log(error);
+// }
